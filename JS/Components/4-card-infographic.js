20bffe556421be09f9e2ee1282c443ac83484310
Riley Griffis <ragriffis@gmail.com>
2022-09-21 16:40:18 -0400

    Updated Landing page back to old design
    Also tweaked the media query for the img grid to go sideways on mobile screens

---
+++ JS/Components/4-card-infographic.js
@@ -0,0 +1,80 @@
+<!------------------------------------------------------START-------------------------------------------------------------1.2-->
+                            
+<div class="fl-row fl-row-bg-none fl-node-59250bcd95b8c" data-node="59250bcd95b8c">
+    <div class="fl-row-content-wrap">
+
+    <div class="uabb-row-separator uabb-top-row-separator" >
+    </div>
+
+    <div class="fl-row-content  fl-node-content" style="padding-bottom: 40px;">    
+        <div class="fl-col-group fl-node-58b5549544737" data-node="58b5549544737">
+        <div class="fl-col fl-node-58b5549544772" data-node="58b5549544772">
+            <div class="fl-col-content fl-node-content">
+
+
+                <h2 class="heading-title" style="text-align: center;">
+                    <span class="title-text pp-primary-title">Why choose snap booth photos?</span>     
+                    </h2>
+
+
+
+                <ul class="cards">
+                    <li class="cards__item">
+                    <div class="card">
+                        <div class="card__image card__image--fence"></div>
+                        <div class="card__content">
+                        <div class="card__title">Stress free set-up</div>
+                        <p class="card__text">Never worry about set-up or tear down! We ensure early set-up free of charge to make sure everything goes according to plan.</p>
+                        </div>
+                    </div>
+                    </li>
+                    <li class="cards__item">
+                    <div class="card">
+                        <div class="card__image card__image--river"></div>
+                        <div class="card__content">
+                        <div class="card__title">Custom graphics</div>
+                        <p class="card__text">Our graphics and photo strip designs are custom made to your satisfaction and can include anything from company logos to custom hashtags.</p>
+                        </div>
+                    </div>
+                    </li>
+                    <li class="cards__item">
+                        <div class="card">
+                        <div class="card__image card__image--flowers"></div>
+                        <div class="card__content">
+                            <div class="card__title">A bundle of fun!</div>
+                            <p class="card__text">At Snap Booth Photos, our priority is to not only create memories through photos, but have fun doing it! You want a photo booth that brings excitment to your event, and Snap Booth Photos promises to deliver.</p>
+                        </div>
+                        </div>
+                    </li>
+                    <li class="cards__item">
+                    <div class="card">
+                        <div class="card__image card__image--record"></div>
+                        <div class="card__content">
+                        <div class="card__title">Prints for each guest</div>
+                        <p class="card__text">We'll make sure each of your guests gets a copy of memories to last a lifetime! Multiple people in one shot? No problem! We'll make sure everyone in the picture gets their own copy!</p>
+                        </div>
+                    </div>
+                    </li>
+                    
+
+                </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+        </div>
+    </div>
+
+    </div>
+</div>
+<!------------------------------------------------------END--------------------------------------------------------------1.2-->
