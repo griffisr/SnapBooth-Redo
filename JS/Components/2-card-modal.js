22608edecd7986e402c676fa25e7c80e9def571f
Riley Griffis <ragriffis@gmail.com>
2022-09-26 16:10:15 -0400

    Fixing quote page

---
+++ JS/Components/2-card-modal.js
@@ -0,0 +1,34 @@
+ 
+                                              <div class="cards-list">
+
+
+
+                                              <a href="#" class="thumbnail" style="border: none;" >
+                                                <div class="card 1" data-toggle="modal" data-target="#myModal">
+                                                  <div class="card_image">
+                                                    <img id="wedding" src="Images/Logos/darkBackgroundLogo.jpg" alt= "Photo Booth Near Me" />
+                                                  </div>
+                                                  <div class="card_title title-white">
+                                                    <p>HOURLY</p>
+                                                  </div>
+                                                </div>
+
+                                              </a>
+
+                                           
+                                              
+                                              
+
+                                              <a href="#" class="thumbnail" style="border: none;">
+                                                <div class="card 2" data-toggle="modal" data-target="#myModal">
+                                                  <div class="card_image">
+                                                    <img id="wedding" src="Images/Promo/Setup.jpg" alt= "Photo Booth Near Me" />
+                                                  </div>
+                                                  <div class="card_title title-white">
+                                                    <p >ALL INCLUSIVE</p>
+                                                  </div>
+                                                </div>
+                                              </a>
+                                              
+
+                                            </div>
